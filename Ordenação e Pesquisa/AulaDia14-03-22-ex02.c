c6ba3414728f6476328e6f9477415a77d7fd2b97
Erick Melo <mmelo.erick@gmail.com>
2022-03-14 20:03:01 -0300

    selection_sort_char

---
+++ Ordenação e Pesquisa/AulaDia14-03-22-ex02.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#define QTDE 12
+
+int main(){
+    char palavra[QTDE] = "SISTEMASINF";
+    int min, aux;
+
+    for (int i = 0; i < QTDE - 1; i++){
+        min = i;
+        for(int j = i+1; j < QTDE; j++){
+            if (palavra[j] < palavra[min]){
+                min = j;
+            }
+        }
+        aux = palavra[min];
+        palavra[min] = palavra[i];
+        palavra[i] = aux;
+    }
+    for (int i = 0; i < QTDE; i++){
+        printf("%c ", palavra[i]);
+    }
+    return 0;
+}
