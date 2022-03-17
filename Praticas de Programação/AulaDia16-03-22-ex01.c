bfbe5613aa21bbaa965569d1b4cfeb2337b9effe
Erick Melo <mmelo.erick@gmail.com>
2022-03-16 21:31:30 -0300

    Fuso Horario

---
+++ Praticas de Programação/AulaDia16-03-22-ex01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#define BRAZIL (-3)
+#define CHINA (+8)
+#include<stdlib.h>
+
+int main(void){
+  time_t currentTime;
+  struct tm *utcTime;
+   currentTime= time(NULL);
+   /* Converte a hora atual para UTC. o ano só é representado por dois caracteres e só temos precisão de minutos.*/
+/*Gmtime é responsável por converter*/
+   utcTime= gmtime(&currentTime);
+   /* Imprime os fuso-horarios do Brasil e China. */
+printf("Brazil: %02d:%02d:%02d\n",(utcTime->tm_hour+BRAZIL)%24*-3,utcTime->tm_min, utcTime->tm_sec);
+printf("China: %02d:%02d:%02d\n", (utcTime->tm_hour+CHINA)%24,utcTime->tm_min, utcTime->tm_sec);
+ system("pause");
+  return 0;
+}
